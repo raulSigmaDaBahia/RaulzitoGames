c55ae3b3119772ffa42077c864c1c872e7e7a55e
Raul sigma da bahia <raulalmeida10anos@gmail.com>
2024-11-26 20:53:57 -0300

    Add files via upload

---
+++ RaulzitoGames/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Videogame',
+    'Melhor jogo',
+    'Red Dead 2'
+)
+
+criaCartao(
+    'Comida',
+    'Melhor comida',
+    'Parmegiana'
+)
+
+criaCartao(
+    'Política',
+    'Melhor presidente',
+    'Mitinho'
+)
+
+criaCartao(
+    'Filme',
+    'Melhor filme',
+    'Star Wars'
+)
